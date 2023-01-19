136b2245548e4e5eb45d50c79239661a011f2d87
Anthony3064 <anthonyestiven3064@gmail.com>
2023-01-19 14:42:33 -0600

    He hecho mis cambios

--- src/main/java/com/cci/test/git/Main.java
+++ src/main/java/com/cci/test/git/Main.java
@@ -14,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World en rama dev");
+        System.out.println("Esto es un ejemplo en la rama dev");
     }
     
 }
